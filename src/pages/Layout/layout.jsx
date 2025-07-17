414f5cece0339a1660889676d0d93ca30c97e204
abdumajid2 <abdumajidt89@gmail.com>
2025-07-18 00:13:03 +0500

    majid

--- src/pages/Layout/layout.jsx
+++ src/pages/Layout/layout.jsx
@@ -50,11 +50,11 @@ const Layout = () => {
                     <FaTh /> Каталог
                   </button>
                   <div className='flex items-center w-[600px] pl-2 justify-between border border-gray-400 rounded-md '>
-                    <input type="text" placeholder='Поиск'/>
-                    <i class="fa-solid bg-blue-500 p-2 rounded-md fa-magnifying-glass"></i>
+                    <input type="text" className='outline-hidden w-full' placeholder='Поиск'/>
+                    <i class="fa-solid bg-blue-500 text-white p-3 rounded-md fa-magnifying-glass"></i>
                   </div>
                   <div className='flex items-center gap-2'>
-                    <i class="fa-solid fa-location-dot"></i>
+                    <i class="fa-solid text-blue-600 fa-location-dot"></i>
                     <p>Иркутск</p>
                     <i class="fa-solid fa-angle-down"></i>
                   </div>
@@ -65,8 +65,59 @@ const Layout = () => {
                 <Outlet/>
             </main>
 
-            <footer>
-
+            <footer className='bg-[#1E2126] flex justify-center p-10 items-center text-white'>
+               <section className='flex justify-between gap-[3rem] '>
+                <article className='flex flex-col gap-6 p-5'>
+                  <div className='flex items-center gap-2'>
+                    <img src="/images/logosh.svg" alt="" />
+                    <h2 className='text-3xl font-bold'>Поставьщик</h2>
+                        </div>
+                        <p>8-800-550-01-09</p>
+                        <span className='text-gray-400'>postav.irk@mail.ru</span>
+                        <div className='flex items-center gap-3 text-2xl text-blue-500'>
+                          <i className='fa-brands fa-telegram'></i>
+                          <i className='fa-brands fa-whatsapp'></i>
+                          <i className='fa-brands fa-vk'></i>
+                          <i className='fa-brands fa-instagram'></i>
+                        </div>
+                        <span className='text-gray-400'>© 2023 все права защищены</span>
+                </article>
+                <div className='border-[1.5px] border-gray-500'></div>
+                <article className='flex flex-col gap-[3rem] p-5'>
+                  <h2>
+                    Информация
+                  </h2>
+                  <div className='flex flex-col gap-5'>
+                    <p>Оплата</p>
+                    <p>Доставка</p>
+                    <p>Политика обработки персональных данных</p>
+                    <p>Согласие на обработку персональных данных</p>
+                  </div>
+                </article>
+                <article className='flex flex-col gap-[3rem] p-5'>
+                  <h2>
+                    Центральный офис и склад
+                  </h2>
+                  <div className='flex flex-col gap-5'>
+                    <div className='flex items-center gap-2'>
+                     <i class="fa-solid text-blue-500 fa-location-dot"></i>
+                     <p>Иркутск, ул. Ленина 123</p>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                      <i class="fa-solid text-blue-500 fa-phone"></i>
+                      <p>8-800-550-01-09</p>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                      <i class="fa-solid text-blue-500 fa-clock"></i>
+                      <p>Пн-Пт 8:00-18:00</p>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                      <i class="fa-solid text-blue-500 fa-building-columns"></i>
+                      <p>Региональные склады</p>
+                    </div>
+                  </div>
+                </article>
+               </section>
             </footer>
         </section>
     </div>
